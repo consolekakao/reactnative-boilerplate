ad35d0d27980b87f5038632af878cf7b92a584d7
consolekakao <console@kakao.com>
2023-01-01 22:16:49 +0900

    first init

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React, {type PropsWithChildren} from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={{backgroundColor: 'red'}}>
+      <Text>Hello</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
